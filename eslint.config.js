53a09ae5bc3d08a9a5894b1fe8e42cdf15f3be1d
rajatv7865v <rajatv785v@gmail.com>
2024-10-19 13:56:24 +0530

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+// // eslint.config.js
+// // import js from '@eslint/js';
+
+// export default [
+//   js.configs.recommended, // Base configuration from ESLint
+//   {
+//     files: ['**/*.js', '**/*.ts'], // Specify file extensions for linting
+//     rules: {
+//       'no-console': 'warn', // Example rule, adjust as needed
+//       semi: ['error', 'always'], // Example rule, adjust as needed
+//     },
+//   },
+// ];
